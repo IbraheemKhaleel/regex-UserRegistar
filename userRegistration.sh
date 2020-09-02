20a157a736b8ae656048d36a697a945927cce064
Ibraheem Khaleel <ikhaleelem@gmail.com>
2020-09-02 12:48:04 +0530

    added code for first valid name

---
+++ userRegistration.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+read -p "Enter your name " name
+rejax=^[A-Z]{1}[a-z]{2}
+if [[ $name =~ $rejax ]]
+then
+	echo "$name is a valid name "
+else
+	echo "name must have first alphabet capital and minimum 3 alphabets "
+fi
