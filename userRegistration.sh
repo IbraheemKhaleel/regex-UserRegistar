25b30f6c7ffc9a0ccdfc5b0f6fcb05f966a39266
Ibraheem Khaleel <ikhaleelem@gmail.com>
2020-09-02 16:50:29 +0530

    added for atleast one number

---
+++ userRegistration.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+read -p "Enter your password " password
+if [[ ${#password} -ge 8 && "$password" == *[A-Z]* && "$password" == *[a-z]* && "$password" == *[0-9]* ]]
+then
+        echo "$password is valid "
+else
+        echo "password must contain atleast a number, capital letter and 8 characters "
+fi
